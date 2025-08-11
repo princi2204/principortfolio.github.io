69243871e3aa6ee9e091611d13dc85c4bc5ecdbd
princi2204 <princi2204@gmail.com>
2025-08-11 14:42:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["3D Modeler", "3D Renderer" ,"Professional in art and craft],
+  strings: ["3D Modeler", "3D Renderer" ,"Professional in art and craft"],
   typeSpeed: 70,
   backSpeed: 40,
   backDelay: 40,
@@ -44,4 +44,5 @@ const swiper = new Swiper(".wrapper", {
     },
   },
 });
+
 
