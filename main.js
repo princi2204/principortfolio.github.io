a9b505ee4d11b95261490c8cdc5d3da97c252582
princi2204 <princi2204@gmail.com>
2025-08-11 13:00:33 +0530

    upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var typed = new Typed(".text", {
+  strings: ["3D Modeling", "3D Visualizer", "Art and Craft"],
+  typeSpeed: 70,
+  backSpeed: 40,
+  backDelay: 40,
+  loop: true,
+});
+
+const swiper = new Swiper(".wrapper", {
+  loop: true,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    736: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 4,
+    },
+  },
+});
